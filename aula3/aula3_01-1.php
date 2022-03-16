c87124b84a9f1b3b8258bb44c28275acb87905ba
Hebert <hebertphp@gmail.com>
2022-03-16 20:46:07 -0300

    PBE_4a_N

---
+++ aula3/aula3_01-1.php
@@ -0,0 +1,14 @@
+<?php
+  $dia = 22;//
+  $mes = "Março";//
+  $ano = 2022;//
+
+  $anocompleto = $dia . " de " . $mes . " de " . $ano;
+  echo $anocompleto;
+  /*
+  http://localhost/backend/  (Caminho para testar os códigos)
+  Saída:
+    São Paulo, 22 de Março de 2022
+
+  */
+?>
