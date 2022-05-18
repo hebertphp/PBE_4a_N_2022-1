0a885c5138d7efbc718d8884c915df76993af5f4
Hebert <hebertphp@gmail.com>
2022-05-18 20:44:48 -0300

    PBE_4a_N

---
+++ aula6/aula6_08-1.php
@@ -0,0 +1,6 @@
+<?php
+$uf = array("SP","RJ","MG","ES","TO","AM","PA");
+foreach ($uf as $valor){
+ echo $valor . "<br>";
+}
+?>
---
+++ aula6/aula6_08-2.php
@@ -0,0 +1,11 @@
+<?php
+$aluno=array("Thiago","Ilvan","Renan","Giovani","Guilherme","Eric","Jabson","Daniel","Kaique","Klisman","Hebert","Jonathan","Andrey");
+
+//echo count($aluno)."<hr>";
+
+for($i=0;$i<count($aluno);$i++){
+    echo $aluno[$i]."<br>";
+}
+
+
+?>
