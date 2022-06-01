a4178f0c028de63d75d9a64dcf290b68aa642b1c
Hebert <hebertphp@gmail.com>
2022-06-01 20:19:33 -0300

    PBE_4a_N

---
+++ aula7/aula7_02.php
@@ -0,0 +1,9 @@
+<?php
+phpinfo();
+echo "<hr>";
+echo "Informações do navegador/SO: ".$_SERVER['HTTP_USER_AGENT'];
+echo "<br>Nome do arquivo: " . $_SERVER['PHP_SELF'];
+echo "<br>Method: ".$_SERVER["REQUEST_METHOD"];
+echo "<hr>";
+echo "<pre>";print_r($_SERVER);echo "</pre>";
+?>
